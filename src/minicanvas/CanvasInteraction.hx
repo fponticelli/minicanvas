3e7a415324539cfed2969880e7cc57713e4aa45a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-08 23:23:27 -0700

    updated canvas interaction methods

--- src/minicanvas/CanvasInteraction.hx
+++ src/minicanvas/CanvasInteraction.hx
@@ -2,35 +2,34 @@ package minicanvas;
 
 using thx.core.Floats;
 
-class CanvasInteraction {
-  var mini : MiniCanvas;
+class CanvasInteraction extends Interaction {
   var x : Float;
   var y : Float;
   var stack : Array<Void -> Void>;
   var _done : Array<Void -> Void> -> Void;
   public function new(mini : MiniCanvas, x : Float, y : Float, done : Array<Void -> Void> -> Void) {
-    this.mini = mini;
+    super(mini);
     this.x = x;
     this.y = y;
     this.stack = [];
     this._done = done;
   }
 
-  public function click(x : Float, y : Float) {
+  override public function click(x : Float, y : Float) {
     if(this.x != x || this.y != y)
       move(x, y);
     stack.push(mini.click.bind(x,y));
     return this;
   }
 
-  public function down(x : Float, y : Float) {
+  override public function down(x : Float, y : Float) {
     if(this.x != x || this.y != y)
       move(x, y);
     stack.push(mini.down.bind(x,y));
     return this;
   }
 
-  public function move(x : Float, y : Float, ?delta : Float = 9) {
+  override public function move(x : Float, y : Float, ?delta : Float = 9) {
     var dist  = Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y)),
         steps = Math.ceil(dist / delta),
         dx, dy, step;
@@ -46,20 +45,20 @@ class CanvasInteraction {
     return this;
   }
 
-  public function up(x : Float, y : Float) {
+  override public function up(x : Float, y : Float) {
     if(this.x != x || this.y != y)
       move(x, y);
     stack.push(mini.up.bind(x,y));
     return this;
   }
 
-  public function sleep(frames : Int) {
+  override public function sleep(frames : Int) {
     for(i in 0...frames)
       stack.push(function(){});
     return this;
   }
 
-  public function done() {
+  override public function done() {
     _done(stack);
     return mini;
   }
