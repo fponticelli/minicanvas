321ed687e558e939a0c4e8c25e709ac4ea6d0fa7
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-08 23:22:20 -0700

    added base class for interaction

---
+++ src/minicanvas/Interaction.hx
@@ -0,0 +1,25 @@
+package minicanvas;
+
+class Interaction {
+  var mini : MiniCanvas;
+  public function new(mini : MiniCanvas)
+    this.mini = mini;
+
+  public function click(x : Float, y : Float)
+    return this;
+
+  public function down(x : Float, y : Float)
+    return this;
+
+  public function move(x : Float, y : Float, ?delta : Float = 9)
+    return this;
+
+  public function up(x : Float, y : Float)
+    return this;
+
+  public function sleep(frames : Int)
+    return this;
+
+  public function done()
+    return mini;
+}
