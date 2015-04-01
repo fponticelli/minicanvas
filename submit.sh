3df935429d3480752945db19f5e49462e71131e4
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:10:26 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm minicanvas.zip
-zip -r minicanvas.zip src extraParams.hxml haxelib.json README.md
+zip -r minicanvas.zip src extraParams.hxml haxelib.json README.md -x "*/\.*"
 haxelib submit minicanvas.zip
